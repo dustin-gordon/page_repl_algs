1691e6a8e0febb787bcceeb18702db7017ae9434
joseemayo <joseemayo2@gmail.com>
2020-04-23 14:38:26 -0500

    Update project2.py

--- project2.py
+++ project2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #################
 #   Program 2   #
 #################
@@ -9,7 +10,7 @@ Dustin Gordon (dmg210)
 Jose Mayorga (jmm498)
 """
 import random
-import Queue
+#import Queue
 
 # initialize variables
 REF_STR_SIZE = 100
@@ -24,7 +25,7 @@ opt_faults = 0
 
 # generate random page reference string
 def create_ref_string():
-    global page_ref_str
+    #global page_ref_str
     counter = 0
     while counter < REF_STR_SIZE:
         rand_int = random.randint(0, 49)
@@ -32,58 +33,54 @@ def create_ref_string():
         counter += 1
 # -------------------------------------
 
-
 # display contents of page reference string
 def print_ref_string():
-    print 'Page Reference String:'
+    print('Page Reference String:')
     for page in page_ref_str:
-        print str(page) + ',',
-    print ''
+        print(str(page) + ',')
+    print('')
 # -----------------------------------------
-
 
 # FIFO Algorithm (First In, First Out)
 # TODO: implement FIFO
 def fifo():
+
     global page_ref_str
     global page_frames
     global fifo_faults
-    fifo_q = Queue.Queue(REF_STR_SIZE)
-
-    for i in range(REF_STR_SIZE):
-        fifo_q.put(page_ref_str[i])  # copy page references to FIFO queue
-
+    fifo_list = list(page_ref_str)  # Make an unlinke copy
+    page_frames_amt = 1
     for ref in page_ref_str:         # iterate page reference list
-        print '\n** Next page ref = ' + str(ref) + ' **'  # debug
+        #print('\n** Next page ref = ' + str(ref) + ' **')  # debug
         page_found = False
         for index in range(page_frames_amt):       # look for page in active frames
             if ref == page_frames[index]:            # page found in existing frame
-                print 'Page found! ref = ' + str(ref) + '; ' + ' frame ' + str(index) + ' = ' + str(page_frames[index])  # debug
+                print('Page found! ref = ' + str(ref) + '; ' + ' frame ' + str(index) + ' = ' + str(page_frames[index]))  # debug
                 page_found = True
                 break
 
-        if not page_found:
+        if not page_found:  # page fault:
             free_frame_found = False
             frame_counter = 0
+            fifo_faults += 1
+            
             for index in range(page_frames_amt):         # look for empty frames
-                if page_frames[index] < 0:               # found a free frame
+                if page_frames[index] == 0:               # found a free frame
                     page_frames[index] = ref  # replace free frame
-                    print 'Replaced free frame ' + str(frame_counter) + ' with page ' + str(ref)  # debug
+                    print('Replaced free frame ' + str(frame_counter) + ' with page ' + str(ref))  # debug
                     free_frame_found = True
                     break
                 frame_counter += 1
+                
             if not free_frame_found:  # page fault: need to replace a non-empty frame via FIFO
-                fifo_faults += 1
-                print 'Page fault! FIFO faults = ' + str(fifo_faults)  # debug
-                frame_to_swap = fifo_q.get()  # pop frame to be swapped from FIFO queue
-                print 'Next page in FIFO queue to swap is ' + str(frame_to_swap)
+                frame_to_swap = fifo_list.pop(0)   # pop frame to be swapped from FIFO queue
+                print('Page fault! FIFO faults = ' + str(fifo_faults) + ' Next page in FIFO queue to swap is ' + str(frame_to_swap))
                 for index in range(page_frames_amt):
                     if page_frames[index] == frame_to_swap:   # found frame to be swapped
-                        print 'Replaced existing frame ' + str(index) + ' = ' + str(page_frames[index]) + '',  # debug
                         page_frames[index] = ref
-                        print ' with page ' + str(ref)  # debug
+                        print('Replaced existing frame ' + str(index) + ' = ' + str(page_frames[index]) + ' with page ' + str(ref))  # debug
 
-    print '\nPage faults using FIFO: ' + str(fifo_faults)
+    print('\nPage faults using FIFO: ' + str(fifo_faults))
 # ------------------------------------
 
 
@@ -91,7 +88,7 @@ def fifo():
 # TODO: implement LRU
 def lru():
     global lru_faults
-    print '\nPage faults using LRU: ' + str(lru_faults)
+    print('\nPage faults using LRU: ' + str(lru_faults))
 # ----------------------------------
 
 
@@ -99,7 +96,7 @@ def lru():
 # TODO: implement OPT
 def opt():
     global opt_faults
-    print '\nPage faults using OPT: ' + str(opt_faults)
+    print('\nPage faults using OPT: ' + str(opt_faults))
 # -----------------------
 
 
@@ -134,10 +131,11 @@ print 'Number of page frames = ' + str(page_frames_amt)
 
 # initialize page frames
 counter = 1
+page_frames_amt = 1
 for frame in range(page_frames_amt):
     temp = counter * -1  # debug
     page_frames.append(temp)  # -1 == empty
-    print 'Frame ' + str(counter) + ' = ' + str(temp)  # debug
+    print('Frame ' + str(counter) + ' = ' + str(temp))  # debug
     counter += 1
 
 # ----------------------
